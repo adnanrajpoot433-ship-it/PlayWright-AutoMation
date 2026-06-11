ba789c09e2f20bb9a89f72bea1d221ab180abea4
Adnan <adnanrajpoot433@gmail.com>
2026-06-11 15:20:02 +0500

    infinite scrolling

---
+++ tests/infinitescrolling-findBook.spec.ts
@@ -0,0 +1,66 @@
+//infinite scrolling cant handle by Playwright so it is handle by Logic
+
+import{test,expect} from "@playwright/test";
+
+test("Infinite scrolling", async({page})=>{
+
+
+test.slow(); // set timeout for a single test easy way to triple the default timeout
+await page.goto("https://www.booksbykilo.in/new-books");
+
+// for scorlling we use javascript statement 
+// window.scrollTo(0,document.body.scrollHeight) (java script statement )
+
+let previousHeight=0;
+let bookFound=false;
+while(true){ 
+
+
+   const titles= await page.locator('#productsDiv h3').allTextContents();
+   if(titles.includes('The Blue Eye'))
+   {
+console.log("Book Found");
+bookFound=true;
+expect(bookFound).toBeTruthy();
+break;
+
+   }
+
+
+    // will scroll down the page
+    await page.evaluate(() =>{
+        window.scrollTo(0,document.body.scrollHeight);
+
+
+
+    })
+   
+ const currentHeight=await page.evaluate (()=>
+    {
+        
+       
+        //capture the current height of the page
+       return document.body.scrollHeight;
+
+    })
+
+    console.log("Previous height", previousHeight);
+    console.log("Current height", currentHeight);
+
+    if(currentHeight===previousHeight){
+
+        break;
+    }
+
+    previousHeight=currentHeight;
+    
+}
+
+console.log("Reached end of the Page :")
+
+if(!bookFound)
+{
+    console.log("Book Not Found");
+}
+  
+})
