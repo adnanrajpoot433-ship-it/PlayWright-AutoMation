b4fd1ff83e16519c5c6c4a1dcbc169cf446449f3
Adnan <adnanrajpoot433@gmail.com>
2026-06-14 12:30:58 +0500

    update tests

--- tests/Hooks2.spec.ts
+++ tests/Hooks2.spec.ts
@@ -1,12 +1,21 @@
 // open app >>>> before all()
 //login
-// find products
+//      find products
 //logout
 
 //login
     //add products to cart
 //logout
 //close app
+
+
+
+
+//if we makes the test in the group and hooks are outside the tests then
+// we can also use those with the test inside the groups,but if we includes
+// the hooks inside the test then those hooks will applicable for that group
+// other tests outside that group will not applicable.
+// so best pratics is that keeps hooks outside group
 
 import {test,expect,Page} from "@playwright/test"
 
@@ -43,7 +52,7 @@ test.afterEach("Log out",async()=>{
 test("Find no of products",async()=>{
 
 const Products=page.locator("#tbodyid .hrefch");
- const count=await Products.count();
+ const count=await Products.count(); //this will return the total products
  console.log("Number of products",count);
  await expect(Products).toHaveCount(9);
 
@@ -55,7 +64,6 @@ test("Add to products",async()=>{
     await page.locator("text='Samsung galaxy s6'").click();
 
     //Handle alert before click 
-
     page.once("dialog",async(dialog)=>{
 
         expect(dialog.message()).toContain("Product added");
