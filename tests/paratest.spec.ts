bd1f9b5d88bd0ff46d952c339ec86d3793227111
Adnan <adnanrajpoot433@gmail.com>
2026-06-15 15:14:05 +0500

    Parametrization

---
+++ tests/paratest.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from "@playwright/test";
+
+//testdata
+
+const searchItems: string[] = ['laptop', 'Gift card', 'smartphone', 'monitor'];
+
+//using for of-loop
+for (const item of searchItems) {
+
+    test(`search test for ${item}`, async ({ page }) => {
+
+        await page.goto("https://demowebshop.tricentis.com/");
+
+        await page.locator("#small-searchterms").fill(item);
+
+        await page.locator("input[value='Search']").click();
+
+        await expect.soft(
+            page.locator("h2 a").nth(0)
+        ).toContainText(item, { ignoreCase: true });
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
